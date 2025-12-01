a8502319fd3fbe2ae0c6287ad1565053159f4fd0
sandersaldev <developer.sandersal@gmail.com>
2025-12-01 18:51:17 -0300

    fix: corrigido base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vivianesolucoesfinanceiras/", // <-- ESSENCIAL!
   plugins: [react()],
-  base: '/vivianesolucoesfinanceiras/', // Caminho base para GitHub Pages
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
