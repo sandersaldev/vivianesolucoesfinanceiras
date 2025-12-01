fdcb836bd70f78852dc43c7f7c765f1700c20da2
sandersaldev <developer.sandersal@gmail.com>
2025-12-01 19:30:57 -0300

    fix: adicionando basename do router para GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/vivianesolucoesfinanceiras">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
