c753b1e0092aadc307e0b3a4e61369e15243912b
sandersaldev <developer.sandersal@gmail.com>
2025-12-01 20:03:10 -0300

    fix: reimportar index.css para corrigir estilos

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
+// 👇 IMPORTANTE: importar o CSS global gerado pelo Lovable/Tailwind
+import "./index.css";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
